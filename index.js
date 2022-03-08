01afb1369e763c87c040d8fb0c192809adc9ffc4
Kent Tsou <tsoukent97@gmail.com>
2022-03-08 13:19:39 +1300

    documents cleaned up

--- index.js
+++ index.js
@@ -15,30 +15,6 @@ document.addEventListener("DOMContentLoaded", ()=> {
     })
 })
 
-// function goToCreate() {
-//     var signupElement = document.getElementById("signup-container");
-//     var loginElement = document.getElementById("login-container");    
-//     console.log(loginElement)
-//     if (signupElement.style.display === "none") {
-//         signupElement.style.display = "block";
-//         loginElement.style.display= "none"
-//       } else {
-//         signupElement.style.display = "none";
-//         loginElement.style.display= "block"
-//       }
-// }
-
-// function goToSignUp() {
-//     login.style.display="block";
-//     createAccount.style.display="none";
-//     var x = document.getElementById("login-container");
-//     if (x.style.display === "none") {
-//         x.style.display = "block";
-//       } else {
-//         x.style.display = "none";
-//       }
-// }
-
 
 
 
